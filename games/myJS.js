01b66544e37a9e7c0a08bb92f1a73c572b20b16b
Pramesti Hatta K <opam.qweiop@gmail.com>
2016-04-01 10:18:19 +0700

    jquery form validation

--- games/myJS.js
+++ games/myJS.js
@@ -1,8 +1,9 @@
 alert('It is the zombie apocalypse. you are looting a store and suddenly a zombie bursts through the door!');
 var weapon = prompt('You search around frantically for a weapon. What do you choose?');
-var randomNumber = Math.round(Math.random());
 
 alert('You attack the zombie with your ' + weapon);
+
+var randomNumber = Math.round(Math.random());
 
 if (randomNumber === 0) {
 	alert('The zombie bites you. You lose!!!');
